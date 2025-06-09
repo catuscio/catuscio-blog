387c551d4232991a6194836646c5957edd83f6da
catuscio <catuscio@hotmail.com>
2025-06-09 23:10:13 +0900

    Deduct duplicated statements

--- src/routes/Feed/PostList/PinnedPosts.tsx
+++ src/routes/Feed/PostList/PinnedPosts.tsx
@@ -4,7 +4,6 @@ import usePostsQuery from "src/hooks/usePostsQuery"
 import styled from "@emotion/styled"
 import { filterPosts } from "./filterPosts"
 import { DEFAULT_CATEGORY } from "src/constants"
-import { useState, useEffect } from "react"  
 import { getPosts } from "src/apis/notion-client/getPosts"  
 
 
